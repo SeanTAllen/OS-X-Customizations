d68a466fa3c589719b8a7a878d030bc68489a7dc
Sean T Allen <sean@monkeysnatchbanana.com>
2011-10-16 23:19:22 -0400

    Update tcp segment size.

--- customize.sh
+++ customize.sh
@@ -5,3 +5,6 @@ defaults write com.apple.dashboard mcx-disabled -boolean YES
 
 # turn off creation of .DS_Store on network drives
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+# tcp segment size to something more 'modern'
+sudo sysctl -w net.inet.tcp.mssdflt=1460
