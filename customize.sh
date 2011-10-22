d711067d69506feb138dd75bde30b2541e69ce45
Sean T Allen <sean@monkeysnatchbanana.com>
2011-10-22 10:43:09 -0400

    Adds some dock 'killing' customizations.

--- customize.sh
+++ customize.sh
@@ -3,6 +3,14 @@
 # turn off dashboard - turn back on w/ boolean NO
 defaults write com.apple.dashboard mcx-disabled -boolean YES 
 
+### make the dock as unobtrusive as possible
+# defaults write com.apple.dock tilesize -int 1
+# defaults write com.apple.dock pinning -string start
+
+### removes the dock entirely
+sudo rm -rf /System/Library/CoreServices/Dock.app
+killall Dock
+
 # turn off creation of .DS_Store on network drives
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
