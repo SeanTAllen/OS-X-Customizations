0502bfee13c4756aa682ad6a83dcfb76d44683df
Sean T Allen <sean@monkeysnatchbanana.com>
2012-10-07 00:16:09 -0400

    Add setting my preferred desktop background.

--- customize.sh
+++ customize.sh
@@ -86,6 +86,44 @@ defaults write com.apple.iTunes disablePingSidebar -bool true
 # Disable all the other Ping stuff in iTunes
 defaults write com.apple.iTunes disablePing -bool true
 
+# Set my preferred desktop background
+sudo cp Solarized-Darker.png "/Library/Desktop Pictures/Solid Colors/"
+defaults write com.apple.desktop Background '{default =     {
+       BackgroundColor =         (
+           "0.2549019753932953",
+           "0.4117647111415863",
+           "0.6666666865348816"
+       );
+       Change = Never;
+       ChangePath = "/Library/Desktop Pictures/Solid Colors";
+       ChangeTime = 1800;
+       CollectionString = "Apple Images";
+       DApertureExpanded = 1;
+       DAppleExpanded = 1;
+       DFolders =         (
+           "~/Wallpaper"
+       );
+       DFoldersExpanded = 1;
+       DRSSExpanded = 0;
+       DRSSFeeds =         (
+       );
+       DSKDesktopPrefPane =         {
+           UserFolderPaths =             (
+           );
+       };
+       DiPhotoExpanded = 1;
+       DrawBackgroundColor = 1;
+       ImageFileAlias = <00000000 00d60003 00000000 cae79faa 0000482b 00000000 00079f0c 015f86fa 0000cba2 16b70000 00000920 fffe0000 00005062 7368ffff ffff0001 000c0007 9f0c0007 9e4e0005 2024000e 002a0014 0053006f 006c0061 00720069 007a0065 0064002d 00440061 0072006b 00650072 002e0070 006e0067 000f0012 00080064 00610065 00640061 006c0075 00730012 003a4c69 62726172 792f4465 736b746f 70205069 63747572 65732f53 6f6c6964 20436f6c 6f72732f 536f6c61 72697a65 642d4461 726b6572 2e706e67 00130001 2f00ffff 0000>;
+       ImageFilePath = "/Library/Desktop Pictures/Solid Colors/Solarized-Darker.png";
+       NewChangePath = "/Library/Desktop Pictures/Solid Colors";
+       NewImageFilePath = "/Library/Desktop Pictures/Solid Colors/Solarized-Darker.png";
+       PathIsAperture = 0;
+       PathIsIPhoto = 0;
+       Placement = Crop;
+       Random = 0;
+       TimerPopUpTag = 6;
+   };}'
+
 # Kill affected applications
 for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" > /dev/null 2>&1; done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
