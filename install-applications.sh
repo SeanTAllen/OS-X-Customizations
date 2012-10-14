7b0ac8123ded7940ae3ec8415bd226b665839592
Sean T Allen <sean@monkeysnatchbanana.com>
2012-10-13 22:40:35 -0400

    Add script for installing standard apps via homebrew
    homebrew and homebrew-casks are requirements for this script.
    there are additional binary apps i use that i need to create
    casks for and add.
    need to add standard brew install commands as well.

---
+++ install-applications.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+brew cask install google-chrome 
+brew cask install one-password
+brew cask install adium
+brew cask install alfred
+brew cask install caffeine
+brew cask install dropbox
+brew cask install echofon
+brew cask install firefox
+brew cask install flux
+brew cask install iterm2
+brew cask install sparrow
+
