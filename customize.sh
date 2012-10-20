8423fd90bc30f66179f28139e15bcb0bfdaa3c5a
Sean T Allen <sean@monkeysnatchbanana.com>
2012-10-20 00:48:21 -0400

    Disable auto termination of inactive apps.

--- customize.sh
+++ customize.sh
@@ -110,6 +110,9 @@ defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 # Disable Resume system-wide
 defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 
+# Disable automatic termination of inactive apps
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
 # Set my preferred desktop background
 sudo cp backgrounds/Solarized-Darker.png "/Library/Desktop Pictures/Solid Colors/"
 defaults write com.apple.desktop Background '{default = {ImageFilePath = "/Library/Desktop Pictures/Solid Colors/Solarized-Darker.png"; };}'
