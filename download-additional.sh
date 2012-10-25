9df8a2c86f5ac778e1f717f815fb3a421ab4d3ac
Sean T Allen <sean@monkeysnatchbanana.com>
2012-10-25 03:49:27 -0400

    Add download additional packages script.
    For software that can't yet be installed via brew-cask.
    Most likely, packages that end up using .pkg installers.

---
+++ download-additional.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# jdk7
+wget http://www.java.net/download/jdk7u10/archive/b13/binaries/jdk-7u10-ea-bin-b13-macosx-x86_64-24_oct_2012.dmg
+# virtualbox
+wget http://download.virtualbox.org/virtualbox/4.2.2/VirtualBox-4.2.2-81494-OSX.dmg
+# vagrant
+wget http://files.vagrantup.com/packages/be0bc66efc0c5919e92d8b79e973d9911f2a511f/Vagrant-1.0.5.dmg
+
