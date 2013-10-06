883daceaf9c34a73b14e1d52398779f38894d1a7
Sean T Allen <sean@monkeysnatchbanana.com>
2013-10-06 12:15:31 -0400

    Remove unused applications from install list.

--- install-applications.sh
+++ install-applications.sh
@@ -21,20 +21,16 @@ brew cask install the-unarchiver
 brew cask install sublime-text
 brew cask install boot-x-changer
 brew cask install arq
-brew cask install colloquy
 brew cask install gitx-l
 brew cask install jumpcut
 brew cask install livestation
 brew cask install moom
-brew cask install libre-office
 brew cask install skype
 brew cask install the-unarchiver
-brew cask install time-machine-editor
 brew cask install ireadfast
 brew cask install keyboard-maestro
 brew cask install ripit
 brew cask install paintbrush
-brew cask install menu-bar-filter
 brew cask install changes
 brew cask install candybar
 
