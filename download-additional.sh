839dee8f9b8a5e20b5a63f0052f63789ecf49605
Sean T Allen <sean@monkeysnatchbanana.com>
2016-01-27 00:32:07 -0500

    Remove apps I no longer use

--- download-additional.sh
+++ download-additional.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-# jdk7
-wget http://www.java.net/download/jdk7u10/archive/b13/binaries/jdk-7u10-ea-bin-b13-macosx-x86_64-24_oct_2012.dmg
-# virtualbox
-wget http://download.virtualbox.org/virtualbox/4.2.2/VirtualBox-4.2.2-81494-OSX.dmg
-# vagrant
-wget http://files.vagrantup.com/packages/be0bc66efc0c5919e92d8b79e973d9911f2a511f/Vagrant-1.0.5.dmg
 # pc keyboard hack - remap key codes at the hardware level
 wget http://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-8.0.0.dmg
 # KeyRemap4MacBook - keyboard customizer
 wget http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-7.8.0.dmg
 # choosy - better 'default' browser control
 wget http://downloads.choosyosx.com/choosy_1.0.3.zip
-# fan control
-wget http://www.eidac.de/smcfancontrol/smcfancontrol_2_4.zip
 # app trap - delete crap w/ app
 wget http://onnati.net/apptrap/download/AppTrap1-1-7.zip
-# mountain lion cache cleaner
-wget http://www.northernsoftworks.com/downloads/mlcc.dmg
-# eclipse
-wget http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/SR1/eclipse-java-juno-SR1-macosx-cocoa-x86_64.tar.gz&url=http://ftp.osuosl.org/pub/eclipse/technology/epp/downloads/release/juno/SR1/eclipse-java-juno-SR1-macosx-cocoa-x86_64.tar.gz&mirror_id=272
 # omnigraffle pro
 wget http://www.omnigroup.com/download/latest/omnigrafflepro
-# totalspace - the ultimate grid spaces manager for your Mac
-wget http://downloads.binaryage.com/TotalSpaces-1.0.1.zip
-# xquartz - x windows manager for osx
-wget http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.4.dmg
 
